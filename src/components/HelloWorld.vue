294d9fd959131aba22c27b27e6f479810905ce91
Dragoljub <zivanovic.dragoljub@gmail.com>
2021-12-18 12:01:21 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { QBtn } from 'quasar'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h5>{{ msg }}</h5>
+  <q-btn id="btn" label="Click me" color="grey"  />
+</template>
+
+<style scoped>
+
+</style>
