90317dc06e0598f62dd97a56f64c54d850ef43c0
Dragoljub <zivanovic.dragoljub@gmail.com>
2021-12-19 17:01:50 +0100

    Fix quasar icons

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { Quasar } from "quasar";
 import quasarIconSet from "quasar/icon-set/svg-mdi-v6";
 
 // Import icon libraries
-import "@quasar/extras/roboto-font/roboto-font.css";
-import "@quasar/extras/mdi-v6/mdi-v6.css";
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
 
 // A few examples for animations from Animate.css:
 import "@quasar/extras/animate/fadeIn.css";
@@ -25,6 +29,19 @@ const myApp = createApp(App);
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
   iconSet: quasarIconSet
+  //same as quasar.conf.js file
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
 });
 
 // Assumes you have a <div id="app"></div> in your index.html
